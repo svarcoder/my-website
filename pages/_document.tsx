8acd3a07553b1ac1b4a8900cada8c03a7365230d
svarcoder <charvai1997@gmail.com>
2023-03-04 17:08:32 +0530

    Complete Changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
+				<title>SVARCODER</title>
 				<link rel='shortcut icon' href='/favicon.png' />
 			</Head>
 			<body>
