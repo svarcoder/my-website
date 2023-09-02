f9a5dd45785cb76c29d20a2643e779bd7eb12fc8
svarcoder <charvai1997@gmail.com>
2023-09-02 20:36:45 +0530

    perf: update resume

--- pages/Components/Banner.tsx
+++ pages/Components/Banner.tsx
@@ -5,96 +5,96 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import Image from "next/image";
 
 const Banner = () => {
-	const [loopNum, setLoopNum] = useState<number>(0);
-	const [isDeleting, setIsDeleting] = useState<boolean>(false);
-	const [text, setText] = useState<string>("");
-	const [delta, setDelta] = useState<number>(300 - Math.random() * 100);
+  const [loopNum, setLoopNum] = useState<number>(0);
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const [text, setText] = useState<string>("");
+  const [delta, setDelta] = useState<number>(300 - Math.random() * 100);
 
-	const toRotate: string[] = [
-		"Frontend Developer.",
-		"Web3 Developer.",
-		"Web Developer.",
-	];
-	const period: number = 2000;
+  const toRotate: string[] = [
+    "Frontend Developer.",
+    "Web3 Developer.",
+    "Web Developer.",
+  ];
+  const period: number = 2000;
 
-	useEffect(() => {
-		let ticker = setInterval(() => {
-			tick();
-		}, delta);
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
 
-		return () => {
-			clearInterval(ticker);
-		};
-	}, [text]);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
-	const tick = () => {
-		let i = loopNum % toRotate.length;
-		let fullText = toRotate[i];
-		let updatedText = isDeleting
-			? fullText.substring(0, text.length - 1)
-			: fullText.substring(0, text.length + 1);
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
-		setText(updatedText);
+    setText(updatedText);
 
-		if (isDeleting) {
-			setDelta((prevDelta) => prevDelta / 2);
-		}
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
 
-		if (!isDeleting && updatedText === fullText) {
-			setIsDeleting(true);
-			setDelta(period);
-		} else if (isDeleting && updatedText === "") {
-			setIsDeleting(false);
-			setLoopNum(loopNum + 1);
-			setDelta(500);
-		}
-	};
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
 
-	const handleDownload = async (e: any) => {
-		e.preventDefault();
-		const response = await fetch("/static/Subham's_Resume.pdf");
-		const blob = await response.blob();
-		const url = URL.createObjectURL(blob);
-		const link = document.createElement("a");
-		link.href = url;
-		link.download = "Subham_Char_Resume.pdf";
-		document.body.appendChild(link);
-		link.click();
-		document.body.removeChild(link);
-	};
+  const handleDownload = async (e: any) => {
+    e.preventDefault();
+    const response = await fetch("/static/SUBHAM_CHAR.pdf");
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "SUBHAM_CHAR.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
-	return (
-		<section className='banner' id='home'>
-			<Container>
-				<Row className='aligh-items-center'>
-					<Col xs={12} md={6} xl={7}>
-						<div>
-							<h1>
-								{`Hi! I'm Subham,`} <br />
-								<span className='txt-rotate'>
-									<span className='wrap'>{text}</span>
-								</span>
-							</h1>
-							<p>
-								Front-end Developer, Specialist in Web3 Development, Experience
-								in Blockchain (Dapp) Web Projects. Knowledgeable about many
-								programming language like JavaScript , React, Blockchain.
-							</p>
-							<button onClick={(e) => handleDownload(e)}>
-								Download Resume
-								<ArrowRightCircle size={25} />
-							</button>
-						</div>
-					</Col>
-					<Col xs={12} md={6} xl={5}>
-						<div className='header_mobile_logo'>
-							<Image src={Header_Img} alt='BannerImage' />
-						</div>
-					</Col>
-				</Row>
-			</Container>
-		</section>
-	);
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <div>
+              <h1>
+                {`Hi! I'm Subham,`} <br />
+                <span className="txt-rotate">
+                  <span className="wrap">{text}</span>
+                </span>
+              </h1>
+              <p>
+                Front-end Developer, Specialist in Web3 Development, Experience
+                in Blockchain (Dapp) Web Projects. Knowledgeable about many
+                programming language like JavaScript , React, Blockchain.
+              </p>
+              <button onClick={(e) => handleDownload(e)}>
+                Download Resume
+                <ArrowRightCircle size={25} />
+              </button>
+            </div>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <div className="header_mobile_logo">
+              <Image src={Header_Img} alt="BannerImage" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Banner;
--- pages/Components/Experience.tsx
+++ pages/Components/Experience.tsx
@@ -6,108 +6,126 @@ import About from "./About";
 import Image from "next/image";
 
 const Experience = () => {
-	return (
-		<section className='experience'>
-			<Container>
-				<Row className='align-items-center'>
-					<About />
-					<div id='experience'>
-						<h2>Experience</h2>
+  return (
+    <section className="experience">
+      <Container>
+        <Row className="align-items-center">
+          <About />
+          <div id="experience">
+            <h2>Experience</h2>
 
-						<div className='timeline'>
-							<div className='timeline__event  animated fadeInUp delay-3s timeline__event--type1'>
-								<div className='timeline__event__icon '>
-									<Image src={Company_Img} alt='Experience Image' />
-								</div>
-								<div className='timeline__event__date'>Full Time</div>
-								<div className='timeline__event__content '>
-									<div className='timeline__event__title'>
-										Associate Software Developer
-									</div>
-									<div className='timeline__event__description'>
-										<p>
-											Rapid Innovation <br />
-											Oct 2021 - Present <br />
-											Noida, Uttar Pradesh, India (Remote)
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='timeline__event  animated fadeInUp delay-3s timeline__event--type1'>
-								<div className='timeline__event__icon '>
-									<Image src={Company_Img} alt='Experience Image' />
-								</div>
-								<div className='timeline__event__date'>Full Time</div>
-								<div className='timeline__event__content '>
-									<div className='timeline__event__title'>
-										Website Developer
-									</div>
-									<div className='timeline__event__description'>
-										<p>
-											Techbae Pty Ltd <br />
-											Oct 2020 - Oct 2021 <br />
-											VIC 3012, Australia (Remote)
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='timeline__event animated fadeInUp delay-2s timeline__event--type2'>
-								<div className='timeline__event__icon'>
-									<Image src={Company_Img} alt='Experience Image' />
-								</div>
-								<div className='timeline__event__date'>Internship</div>
-								<div className='timeline__event__content'>
-									<div className='timeline__event__title'>
-										Junior Software Engineer
-									</div>
-									<div className='timeline__event__description'>
-										<p>
-											Nmdsoft Software and Network Pvt Ltd
-											<br />
-											Sep 2020 - Oct 2020 <br />
-											Gurugram, Haryana, India (Remote)
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='timeline__event animated fadeInUp delay-1s timeline__event--type3'>
-								<div className='timeline__event__icon'>
-									<Image src={Intern_Img} alt='Experience Image' />
-								</div>
-								<div className='timeline__event__date'>Intern Ship</div>
-								<div className='timeline__event__content'>
-									<div className='timeline__event__title'>Summer Intern</div>
-									<div className='timeline__event__description'>
-										<p>
-											KVCH <br />
-											Jun 2019 - Aug 2019 <br />
-											Noida, Uttar Pradesh, India (On-Site)
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='timeline__event animated fadeInUp timeline__event--type1'>
-								<div className='timeline__event__icon'>
-									<Image src={Intern_Img} alt='Experience Image' />
-								</div>
-								<div className='timeline__event__date'>Intern Ship</div>
-								<div className='timeline__event__content'>
-									<div className='timeline__event__title'>Summer Intern</div>
-									<div className='timeline__event__description'>
-										<p>
-											Microsoft Technology Associate <br />
-											Jun 2018 - Aug 2018 <br />
-											Lucknow, Uttar Pradesh, India (On-Site)
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</Row>
-			</Container>
-		</section>
-	);
+            <div className="timeline">
+              <div className="timeline__event  animated fadeInUp delay-3s timeline__event--type1">
+                <div className="timeline__event__icon ">
+                  <Image src={Company_Img} alt="Experience Image" />
+                </div>
+                <div className="timeline__event__date">Full Time</div>
+                <div className="timeline__event__content ">
+                  <div className="timeline__event__title">
+                    Senior Software Developer
+                  </div>
+                  <div className="timeline__event__description">
+                    <p>
+                      Comdex <br />
+                      Apr, 2023 - Present <br />
+                      Bengaluru, Karnataka, India (Remote)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="timeline__event  animated fadeInUp delay-3s timeline__event--type1">
+                <div className="timeline__event__icon ">
+                  <Image src={Company_Img} alt="Experience Image" />
+                </div>
+                <div className="timeline__event__date">Full Time</div>
+                <div className="timeline__event__content ">
+                  <div className="timeline__event__title">
+                    Software Developer
+                  </div>
+                  <div className="timeline__event__description">
+                    <p>
+                      Rapid Innovation <br />
+                      Oct, 2021 - Apr, 2023 <br />
+                      Noida, Uttar Pradesh, India (Remote)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="timeline__event  animated fadeInUp delay-3s timeline__event--type1">
+                <div className="timeline__event__icon ">
+                  <Image src={Company_Img} alt="Experience Image" />
+                </div>
+                <div className="timeline__event__date">Full Time</div>
+                <div className="timeline__event__content ">
+                  <div className="timeline__event__title">
+                    Full Stack Developer
+                  </div>
+                  <div className="timeline__event__description">
+                    <p>
+                      Techbae Pty Ltd <br />
+                      Oct, 2020 - Oct, 2021 <br />
+                      VIC 3012, Australia (Remote)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="timeline__event animated fadeInUp delay-2s timeline__event--type2">
+                <div className="timeline__event__icon">
+                  <Image src={Company_Img} alt="Experience Image" />
+                </div>
+                <div className="timeline__event__date">Internship</div>
+                <div className="timeline__event__content">
+                  <div className="timeline__event__title">
+                    Junior Software Engineer
+                  </div>
+                  <div className="timeline__event__description">
+                    <p>
+                      Nmdsoft Software and Network Pvt Ltd
+                      <br />
+                      Sep, 2020 - Oct, 2020 <br />
+                      Gurugram, Haryana, India (Remote)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="timeline__event animated fadeInUp delay-1s timeline__event--type3">
+                <div className="timeline__event__icon">
+                  <Image src={Intern_Img} alt="Experience Image" />
+                </div>
+                <div className="timeline__event__date">Intern Ship</div>
+                <div className="timeline__event__content">
+                  <div className="timeline__event__title">Summer Intern</div>
+                  <div className="timeline__event__description">
+                    <p>
+                      KVCH <br />
+                      Jun, 2019 - Aug, 2019 <br />
+                      Noida, Uttar Pradesh, India (On-Site)
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="timeline__event animated fadeInUp timeline__event--type1">
+                <div className="timeline__event__icon">
+                  <Image src={Intern_Img} alt="Experience Image" />
+                </div>
+                <div className="timeline__event__date">Intern Ship</div>
+                <div className="timeline__event__content">
+                  <div className="timeline__event__title">Summer Intern</div>
+                  <div className="timeline__event__description">
+                    <p>
+                      Microsoft Technology Associate <br />
+                      Jun, 2018 - Aug, 2018 <br />
+                      Lucknow, Uttar Pradesh, India (On-Site)
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Experience;
